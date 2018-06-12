b17397b5c767faf49d3f3cf0dfc8c7fc8198a43f
unifoxdigital <unifoxdigital.hihasan@gmail.com>
2018-06-13 04:24:04 +0600

    Create a thread

--- app/src/main/java/com/nsu/cse299/project/StartupActivity.java
+++ app/src/main/java/com/nsu/cse299/project/StartupActivity.java
@@ -1,18 +1,31 @@
 package com.nsu.cse299.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class StartupActivity extends AppCompatActivity
 
 {
+    public final  int THREAD_RUNNING_TIME=5000;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
 
         checkInternet();
+
+        //Thread Handler for 5 sec.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(StartupActivity.this, MainActivity.class);
+                StartupActivity.this.startActivity(mainIntent);
+                StartupActivity.this.finish();
+            }
+        }, THREAD_RUNNING_TIME);
     }
 
     private void checkInternet()
