7a2e493ccbb6574726052f5125e6f8718759310d
unifoxdigital <unifoxdigital.hihasan@gmail.com>
2018-06-23 10:55:57 +0600

    Some minor changess
    Add acctivity and btn action define

--- app/src/main/java/com/nsu/cse299/project/MainActivity.java
+++ app/src/main/java/com/nsu/cse299/project/MainActivity.java
@@ -1,15 +1,55 @@
 package com.nsu.cse299.project;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private EditText input;
+    private TextView change_txt;
+    private FloatingActionButton fab;
+    private Context context=this;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input=(EditText) findViewById (R.id.temperature_value);
+        change_txt= (TextView) findViewById (R.id.emoticon_txt);
+
+        fab=(FloatingActionButton) findViewById (R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.activity_team);
+                dialog.setTitle("Title...");
+
+                AppCompatButton dialogButton = (AppCompatButton) dialog.findViewById(R.id.cancel);
+                // if button is clicked, close the custom dialog
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
+
+
+
+
     }
 }
